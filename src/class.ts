91318435e4964b641539ece376ade12f6b64250d
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-17 23:32:54 +0300

    tsc class

---
+++ src/class.ts
@@ -0,0 +1,11 @@
+class House {
+    street: string;
+
+    constructor (n: string) {
+        this.street = n;
+    }
+}
+
+const house1 = new House('Middle-earth1');
+const house2 = new House('Middle-earth2');
+const house3 = new House('Middle-earth2');
