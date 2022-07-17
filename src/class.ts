357277b87522a904cfbbee5cd8cc91fa42430fd0
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-17 23:38:10 +0300

    tsc class methods

--- src/class.ts
+++ src/class.ts
@@ -4,8 +4,18 @@ class House {
     constructor (n: string) {
         this.street = n;
     }
+
+    showAddress (this: House, add: string): void {
+        console.log('Address' + this.street + '' + add);
+    }
 }
 
 const house1 = new House('Middle-earth1');
 const house2 = new House('Middle-earth2');
 const house3 = new House('Middle-earth2');
+
+// house1.showAddress();
+
+const copyHouse = { showAddress: house1.showAddress, street: 'New address'};
+
+copyHouse.showAddress('add');
