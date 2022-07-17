c789067c744ad338fa3bed0557a895756eee9775
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-17 23:51:41 +0300

    less class code

--- src/class.ts
+++ src/class.ts
@@ -1,9 +1,15 @@
 class House {
-   private street: string;
+//    private type: string;
+//    private street: string;private street: string
    private tenants: string[] = [];
 
-    constructor (n: string) {
-        this.street = n;
+    // constructor (t: string, n: string) {
+    //     this.street = n;
+    //     this.type = t;
+    // }
+
+    constructor (private type: string, private street: string) {
+        
     }
 
     public  showAddress (this: House, add: string): void {
@@ -11,17 +17,23 @@ class House {
     }
 
     public addTenant (name: string) {
-this.tenants.push(name)
+        this.tenants.push(name)
     }
 
     public showTenant () {
         console.log(this.tenants)
             }
+
+    public showType () {
+        console.log('Type' + this.type)
+ }
 }
 
-const house1 = new House('Middle-earth1');
-const house2 = new House('Middle-earth2');
-const house3 = new House('Middle-earth2');
+// const house1 = new House('Middle-earth1');
+const house1 = new House('wood', 'Middle-earth1');
+
+// const house2 = new House('Middle-earth2');
+// const house3 = new House('Middle-earth2');
 
 // house1.showAddress();
 
