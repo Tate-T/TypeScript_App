8d073aae04602ae85ad4ef3addf904f573be4ccd
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-17 22:59:37 +0300

    TypeScript dataTypes exemples

--- app.js
+++ app.js
@@ -19,3 +19,58 @@ var notInitialize;
 notInitialize = undefined;
 var callback;
 callback = function (a) { return 100 + a; };
+var anything;
+anything = -20;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string') {
+    str = some;
+}
+var person;
+person = ['Max', 21];
+var Toggle;
+(function (Toggle) {
+    Toggle[Toggle["LOADING"] = 0] = "LOADING";
+    Toggle[Toggle["READY"] = 1] = "READY";
+})(Toggle || (Toggle = {}));
+;
+var loading = {
+    status: Toggle.LOADING
+};
+if (loading.status === Toggle.READY) {
+    console.log('READY');
+}
+;
+var union;
+union = 1;
+union = 'string';
+var union2;
+// function showMessage(message: string): void;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: new Date,
+        updateAt: new Date
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close'
+};
--- app.ts
+++ app.ts
@@ -22,3 +22,81 @@ let notInitialize;
 notInitialize = undefined;
 let callback: (a: number) => number;
 callback = (a) => { return 100 + a };
+
+let anything: any;
+ anything = -20;
+anything = 'Text';
+anything = {};
+
+let some:unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') {
+  str = some;
+}
+
+let person: [string, number]
+person = ['Max', 21];
+
+enum Toggle {LOADING, READY};
+
+const loading = {
+  status: Toggle.LOADING
+}
+
+if (loading.status === Toggle.READY) {
+  console.log('READY')
+};
+
+let union: number | string;
+
+union = 1;
+union = 'string';
+
+let union2: 'enable'  | 'disable';
+
+// function showMessage(message: string): void;
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  }
+}
+
+const page1: Page = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date,
+    updateAt: new Date,
+  }
+}
+
+const page2: Page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
